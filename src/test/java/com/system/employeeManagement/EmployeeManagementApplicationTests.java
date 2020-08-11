9d5e64a72029885a7e3c363e4170594f37a82b3b
ujjwal <ujjwal@192.168.1.5>
2020-08-11 19:06:27 +0530

    Adding file.

---
+++ src/test/java/com/system/employeeManagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.employeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
